387de30b2e4817dc24dc51de6a6f18aeb74860f2
Stephen Agneta <you@example.com>
2015-10-16 14:03:33 -0400

    Checkpoint. Somewhat working. Queries starting to work etcetera.

--- README.md
+++ README.md
@@ -4,5 +4,6 @@ Accepts an audit log in stdin and emits an un-opaqued version to stdout
 # To build the jar file at root of project execute the following command without arguments.
 gradle
 
-# To run the application
-java -jar ./build/libs/brigid.jar
+# To run the application...
+java -jar ./build/libs/brigid.jar < ~/tmp/secure-audit.log 
+
--- src/main/java/com/bjond/Main.java
+++ src/main/java/com/bjond/Main.java
@@ -14,6 +14,31 @@
 
 package com.bjond;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.io.Reader;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
+import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.handlers.BeanHandler;
+import org.apache.commons.dbutils.handlers.BeanListHandler;
+import org.apache.commons.lang.StringUtils;
+
+import com.bjond.entities.User;
+
+import lombok.val;
+import lombok.extern.slf4j.Slf4j;
+
 
 /** <p> Main entry point for the application </p>
 
@@ -21,10 +46,100 @@ package com.bjond;
  * <a href="mailto:Stephen.Agneta@bjondinc.com">Steve 'Crash' Agneta</a>
  *
  */
+@Slf4j
+public class Main  {
+    private static final String OPENSHIFT_POSTGRESQL_DB_HOST     = System.getenv("OPENSHIFT_POSTGRESQL_DB_HOST");
+    private static final String OPENSHIFT_POSTGRESQL_DB_PORT     = System.getenv("OPENSHIFT_POSTGRESQL_DB_PORT");
+    private static final String OPENSHIFT_APP_NAME               = System.getenv("OPENSHIFT_APP_NAME");
+    private static final String OPENSHIFT_POSTGRESQL_DB_USERNAME = System.getenv("OPENSHIFT_POSTGRESQL_DB_USERNAME");
+    private static final String OPENSHIFT_POSTGRESQL_DB_PASSWORD = System.getenv("OPENSHIFT_POSTGRESQL_DB_PASSWORD");
 
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    static String POSTGRESQL_URL; 
+    
+    public static void main(String[] args) throws IOException, SQLException {
+        process(System.in, System.out);
     }
+
+
+    private static void process(final InputStream in , final OutputStream out) throws IOException, SQLException {
+        log.info("Execution begins...");
+        
+        // Generate the POSTGRESQL URL form system envirionment variables.
+        POSTGRESQL_URL = String.format("jdbc:postgresql://%s:%s/%s", OPENSHIFT_POSTGRESQL_DB_HOST, OPENSHIFT_POSTGRESQL_DB_PORT, OPENSHIFT_APP_NAME); 
+
+        try(final Connection db = DriverManager.getConnection(POSTGRESQL_URL, OPENSHIFT_POSTGRESQL_DB_USERNAME, OPENSHIFT_POSTGRESQL_DB_PASSWORD);) {
+        
+         
+            final PrintStream outPrintStream = new PrintStream(out, true, "UTF-8");
+            final Reader inReader = new InputStreamReader(in, "UTF-8");
+            final Iterable<CSVRecord> records = CSVFormat.DEFAULT.withQuote('\'').parse(inReader);
+
+            //outPrintStream.printf("%s%n%n", POSTGRESQL_URL);
+            outPrintStream.printf("PostgreSQL DB connectiion valid:  %b%n%n", db.isValid(1000));
+        
+            records.forEach(record -> {
+                    record.iterator().forEachRemaining(e -> {
+
+                            try {
+                                if(e.isEmpty()) {
+                                    outPrintStream.printf("%n"); // EOL
+                                } else {
+                                    //outPrintStream.printf("%s%n", e);
+                                    final String[] tuple = keyValueSplitter(e);
+                                    outPrintStream.printf("%s='%s'  ", tuple[0], resolve(db, tuple[0], tuple[1]));
+                                }
+                            } catch(final Exception exception) {
+                                log.error("unexpected error on " + e, exception);
+                            }
+                        });
+                });
+        }
+
+    }
+
+    
+
+    private static String[] keyValueSplitter(final String element) {
+        // key=value split on equals
+        String[] s = element.split("=");
+
+        // Remove quotes
+        s[1] = StringUtils.remove(s[1], '\'');
+        
+        return s;
+    }
+    
+    
+    private static String resolve(final Connection connection, final String key, final String value) throws SQLException {
+        switch(key) {
+        case "IDENTITY":
+            {
+                final User result = findUseByID(connection, value);
+                return (result != null) ? result.toString() : value;
+            }
+            
+        default: return value;
+        }
+    }
+
+    public static User findUseByID(final Connection connection, final String ID) throws SQLException {
+
+        //System.out.println("ID IS " + ID);
+        val run = new QueryRunner();
+        return run.query(connection, "SELECT p.id, p.loginname, p.firstname, p.lastname, p.email  FROM accounttypeentity p WHERE p.id = ?", new BeanHandler<User>(User.class), ID);
+    }
+
+    
+    public static Set<User> findAllUsers(final Connection connection, final Set<String> ids, final int iLimit, final int iOffset) throws SQLException {
+        if(ids == null || ids.isEmpty()) {
+            return new HashSet<>();
+        }
+        
+        val run = new QueryRunner();
+        val quotedList = ids.stream().map(id -> "'" + id + "'").collect(Collectors.toList());
+        val SQL = String.format("SELECT p.id, p.loginname, p.firstname, p.lastname, p.email  FROM accounttypeentity p WHERE p.id IN (%s) ORDER BY UPPER(p.loginname) ASC LIMIT ? OFFSET ?", StringUtils.join(quotedList, ","));
+        
+        return new HashSet<>(run.query(connection, SQL , new BeanListHandler<User>(User.class), iLimit, iOffset));
+    }
+    
 }
