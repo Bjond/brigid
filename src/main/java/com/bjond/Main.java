b6136490377f0855d1fce8d3fb7d1f147fccbb00
Stephen Agneta <you@example.com>
2015-10-16 09:38:06 -0400

    Basic unit test with test data ready to be filled in later.

--- src/main/java/com/bjond/Main.java
+++ src/main/java/com/bjond/Main.java
@@ -24,8 +24,7 @@ package com.bjond;
 
 
 public class Main {
-  
     public static void main(String[] args) {
-        System.out.println("Hello World!"); 
+        System.out.println("Hello World!");
     }
 }
