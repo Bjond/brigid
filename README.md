0616ff28d5f54b9343cbb73d2fa14533fb41ed0c
Stephen Agneta <you@example.com>
2015-10-16 08:26:38 -0400

    Initial build of the project. Mechanics work properly.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # brigid
 Accepts an audit log in stdin and emits an un-opaqued version to stdout
+
+# To build the jar file at root of project execute the following command without arguments.
+gradle
+
+# To run the application
+java -jar ./build/libs/brigid.jar
---
+++ src/main/java/com/bjond/Main.java
@@ -0,0 +1,31 @@
+/*  Copyright (c) 2015
+ *  by Bjönd Health, Inc., Boston, MA
+ *
+ *  This software is furnished under a license and may be used only in
+ *  accordance with the terms of such license.  This software may not be
+ *  provided or otherwise made available to any other party.  No title to
+ *  nor ownership of the software is hereby transferred.
+ *
+ *  This software is the intellectual property of Bjönd Health, Inc.,
+ *  and is protected by the copyright laws of the United States of America.
+ *  All rights reserved internationally.
+ *
+ */
+
+package com.bjond;
+
+
+/** <p> Main entry point for the application </p>
+
+ *
+ * <a href="mailto:Stephen.Agneta@bjondinc.com">Steve 'Crash' Agneta</a>
+ *
+ */
+
+
+public class Main {
+  
+    public static void main(String[] args) {
+        System.out.println("Hello World!"); 
+    }
+}
